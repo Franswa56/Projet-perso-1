116780f291f6bcd747be8e79808be3feff00dc29
François Philippon <franswa56@hotmail.fr>
2023-10-19 04:51:45 +0200

    Ajout barre de recherche

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function displayElements(elements, i) {
  
     // Crée un nouvel élément "article"
     const article = document.createElement("article");
-    const loader = document.createElement("div");
+    const loader = document.createElement("img");
     loader.className = "loader";
-
-    
-       
+    loader.src = "assets/loader.png"
+      
     // Crée un nouvel élément "img" pour la photo
     const image = new Image();
     image.src = element.sprites.regular;
@@ -34,14 +33,9 @@ function displayElements(elements, i) {
         article.appendChild(image)
         article.appendChild(title);
 
-    }
-       
+    }      
     // Ajoute l'image et le titre à l'élément "article"
-    article.appendChild(image);
-
-   
-    
- 
+    article.appendChild(image); 
     // Ajoute l'élément "article" à la div "gallery"
     gallery.appendChild(article);
 }
@@ -49,6 +43,7 @@ function displayElements(elements, i) {
 
 function displayShinyElements(elements, i) {
     const gallery = document.querySelector(".gallery");
+    const arrowsDisplay = document.querySelector(".arrows");
     gallery.innerHTML = "";
     
     // Obtenez le premier Pokémon
@@ -56,20 +51,33 @@ function displayShinyElements(elements, i) {
  
     // Crée un nouvel élément "article"
     const article = document.createElement("article");
-       
+    const loader = document.createElement("div");
+    loader.className = "loader";
+      
     // Crée un nouvel élément "img" pour la photo
-    const image = document.createElement("img");
+    const image = new Image();
     image.src = element.sprites.shiny;
     image.alt = element.name.fr;
-       
+
+    arrowsDisplay.style.visibility = "hidden";
+    image.style.visibility = "hidden";
+
     // Crée un nouvel élément "p" pour le titre
     const title = document.createElement("p");
     title.innerText = element.name.fr;
-       
+
+    article.appendChild(loader)
+
+    image.onload = function() {
+        article.removeChild(loader);
+        image.style.visibility = "visible";
+        arrowsDisplay.style.visibility = "visible";
+        article.appendChild(image)
+        article.appendChild(title);
+
+    }      
     // Ajoute l'image et le titre à l'élément "article"
-    article.appendChild(image);
-    article.appendChild(title);
- 
+    article.appendChild(image); 
     // Ajoute l'élément "article" à la div "gallery"
     gallery.appendChild(article);
 }
@@ -134,6 +142,72 @@ fullStar.addEventListener("click", ()=> {
 
 displayElements(data, i);
 })
+})
+
+                           // Recherche d'un pokémon //
+// Fonctions 
+
+function displaySearchedElements(element) {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    // Crée un nouvel élément "article"
+    const article = document.createElement("article");
+    const loader = document.createElement("div");
+    loader.className = "loader";
+      
+    // Crée un nouvel élément "img" pour la photo
+    const image = new Image();
+    image.src = element.sprites.regular;
+    image.alt = element.name.fr;
+
+    image.style.visibility = "hidden";
+
+    // Crée un nouvel élément "p" pour le titre
+    const title = document.createElement("p");
+    title.innerText = element.name.fr;
+
+    article.appendChild(loader)
+
+    image.onload = function() {
+        article.removeChild(loader);
+        image.style.visibility = "visible";
+        article.appendChild(image)
+        article.appendChild(title);
+    }      
+    // Ajoute l'image et le titre à l'élément "article"
+    article.appendChild(image); 
+    // Ajoute l'élément "article" à la div "gallery"
+    gallery.appendChild(article);
+}
+
+// Récupération de l'input de l'utilisateur
+
+const searchForm = document.querySelector(".search-form")
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+const pokesearch = searchForm.querySelector("[name=pokemon]").value;
+
+// envoi de l'input a la base de données
+
+fetch(`https://api-pokemon-fr.vercel.app/api/v1/pokemon/${pokesearch}`)
+.then(response => { 
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+})
+.then(data => { 
+    console.log(data);
+    displaySearchedElements(data)
+})
+.catch(error => {
+    // La gestion des erreurs lancées se fait ici.
+    console.error('Fetch Error: ', error);
+    console.log("coucou")
+});
+
+
 })
 
 
